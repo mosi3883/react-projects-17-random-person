edecc2cd4a0ea78da5b6acc544f657f4c03d46ad
Mostafa <mosi3883@gmail.com>
2022-06-03 18:29:47 +0430

    fetch user

--- src/App.js
+++ src/App.js
@@ -10,7 +10,74 @@ import {
 const url = 'https://randomuser.me/api/';
 const defaultImage = 'https://randomuser.me/api/portraits/men/75.jpg';
 function App() {
-  return <h2>random user starter</h2>;
+  const [loading, setLoading] = useState(true);
+  const [person, setPerson] = useState(null);
+  const [title, setTitle] = useState('name');
+  const [value, setValue] = useState('random person');
+
+  const getPerson = async () => {
+    const response = await fetch(url);
+    const data = await response.json();
+    const person = data?.results[0];
+    const newPerson = {
+      name: `${person.name.first} ${person.name.last}`,
+      phone: person.phone,
+      email: person.email,
+      age: person.dob.age,
+      street: `${person.location.street.number} ${person.location.street.name}`,
+      password: person.login.password,
+      image: person.picture.large,
+    };
+    setPerson(newPerson);
+    setLoading(false);
+    setTitle('name');
+    setValue(newPerson.name);
+  };
+
+  useEffect(() => {
+    getPerson();
+  }, []);
+
+  const handleValue = (e) => {
+    const btn = e.target.closest('button');
+    const label = btn.dataset.label;
+  };
+
+  return (
+    <main>
+      <div className='block bcg-black'></div>
+      <div className='block'>
+        <div className='container'>
+          <img src={person?.image ?? defaultImage} alt='random user' className='user-img' />
+          <p className='user-title'>my {title} is</p>
+          <p className='user-value'>{value}</p>
+          <div className='values-list'>
+            <button className='icon' data-label='name' onMouseOver={handleValue}>
+              <FaUser />
+            </button>
+            <button className='icon' data-label='email' onMouseOver={handleValue}>
+              <FaEnvelopeOpen />
+            </button>
+            <button className='icon' data-label='age' onMouseOver={handleValue}>
+              <FaCalendarTimes />
+            </button>
+            <button className='icon' data-label='street' onMouseOver={handleValue}>
+              <FaMap />
+            </button>
+            <button className='icon' data-label='phone' onMouseOver={handleValue}>
+              <FaPhone />
+            </button>
+            <button className='icon' data-label='password' onMouseOver={handleValue}>
+              <FaLock />
+            </button>
+          </div>
+          <button className='btn' type='button' onClick={getPerson}>
+            {loading ? 'loading...' : 'random user'}
+          </button>
+        </div>
+      </div>
+    </main>
+  );
 }
 
 export default App;
